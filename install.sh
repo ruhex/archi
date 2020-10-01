7b2680b0bbd4e21f46f6ef3313a71e3d639c87a5
Mikhail Movchan <devcore.ru@gmail.com>
2020-10-01 09:27:57 +0300

    new install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# This is Arch Linux Installation Script.
+
+echo "Ruhex Arch Installer"
+
+# Set up time
+timedatectl set-ntp true
+
+# Initate pacman keyring
+pacman-key --init
+pacman-key --populate archlinux
+pacman-key --refresh-keys
+
+# to create the partitions
+echo ",500M,U;" | sfdisk /dev/sda 0
+echo ",,,;" | sfdisk /dev/sda 1
+
+# Format the partitions
+mkfs.fat -F32 /dev/sda1
+mkfs.btrfs -L test -n 32k /dev/sda2
+
+
+# Mount the partitions
+mount /dev/sda2 /mnt
+mkdir /boot/EFI
+mount /dev/sda1 /boot/EFI
+
+# Install Arch Linux
+echo "Starting install.."
+echo "Installing Arch Linux, GRUB2 as bootloader and other utils"
+pacstrap /mnt base intel-ucode efibootmgr dosfstools os-prober mtools freetype2 grub sway nano git zip sudo networkmanager openssh
+
+# Generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Copy post-install system cinfiguration script to new /root
+cp -rfv post-install.sh /mnt/root
+chmod a+x /mnt/root/post-install.sh
+
+# Chroot into new system
+echo "Press any key to chroot..."
+read tmpvar
+arch-chroot /mnt /bin/bash
+
+# Finish
+echo "Press any key to reboot or Ctrl+C to cancel..."
+read tmpvar
+reboot
---
+++ post-install.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+# This is Configuration script of Ruhex Arch Linux Installation Package.
+echo "Ruhex Arch Configurator"
+
+# Set date time
+ln -sf /usr/share/zoneinfo/Asia/Vladivostok /etc/localtime
+hwclock --systohc
+
+# Set locale to en_US.UTF-8 UTF-8
+localectl set-locale LANG=en_US.UTF-8
+locale-gen
+
+# Set hostname
+echo "barsik" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1	barsik.localdomain	barsik" >> /etc/hosts
+
+# Generate initramfs
+mkinitcpio -P
+
+# Set root password
+passwd
+
+# Install bootloader
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=arch
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Create new user
+useradd -m -G wheel,power,iput,storage,uucp,network kenny
+sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
+echo "Set password for new user"
+passwd kenny
+
+# Enable services
+systemctl enable NetworkManager.service
+systemctl enable sshd.service
+
+echo "Configuration done. You can now exit chroot."
