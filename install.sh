96fd61958a49f8ba8984c8d3f8b7f4b597468f5a
Mikhail Movchan <devcore.ru@gmail.com>
2020-10-01 11:25:41 +0300

    change fdisk to sfdisk

--- install.sh
+++ install.sh
@@ -13,32 +13,9 @@ pacman-key --populate archlinux
 pacman-key --refresh-keys
 
 # to create the partitions
-#echo ',,L' | sfdisk --wipe=always --label=gpt /dev/sda
-#echo ",500M,U;" | sfdisk /dev/sda 0
-#echo ",,,;" | sfdisk /dev/sda 1
-
-# to create the partitions programatically (rather than manually)
-# https://superuser.com/a/984637
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
-  g # GPT lable
-  o # clear the in memory partition table
-  n # new partition
-  p # primary partition
-  1 # partition number 1
-    # default - start at beginning of disk 
-  +512M # 512 MB boot parttion
-  n # new partition
-  p # primary partition
-  2 # partion number 2
-    # default, start immediately after preceding partition
-    # default, extend partition to end of disk
-  a # make a partition bootable
-  1 # bootable partition is partition 1 -- /dev/sda1
-  p # print the in-memory partition table
-  w # write the partition table
-  q # and we're done
-EOF
-
+echo ',,L' | sfdisk --wipe=always --label=gpt /dev/sda
+echo ",500M,U;" | sfdisk /dev/sda 0
+echo ",,,;" | sfdisk /dev/sda 1
 
 # Format the partitions
 mkfs.fat -F32 /dev/sda1
